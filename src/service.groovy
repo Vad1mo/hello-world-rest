2c171165aa50d651d5815a8b5120249e59d9a2f0
Vadim Bauer <bauer.vadim@gmail.com>
2017-01-06 22:11:18 +0100

    add host

--- src/service.groovy
+++ src/service.groovy
@@ -8,16 +8,16 @@ import static ratpack.groovy.Groovy.ratpack
 ratpack {
     handlers {
         get {
-            render "/ - Hello World!"
+            render "/ - Hello World! Host:${InetAddress.getLocalHost()}"
         }
         get(":name") {
-            render "/ - Hello $pathTokens.name!"
+            render "/ - Hello $pathTokens.name! Host:${InetAddress.getLocalHost()}"
         }
         get("/:path1/:path2") {
-            render "/:path1/:path2 - Hello to $pathTokens.path1/$pathTokens.path2 !"
+            render "/:path1/:path2 - Hello to $pathTokens.path1/$pathTokens.path2 ! Host:${InetAddress.getLocalHost()}"
         }
         get("/:path1/:path2/:path3") {
-            render "/:path1/:path2/:path3 - Hello to $pathTokens.path1/$pathTokens.path2/$pathTokens.path3 !"
+            render "/:path1/:path2/:path3 - Hello to $pathTokens.path1/$pathTokens.path2/$pathTokens.path3 ! Host:${InetAddress.getLocalHost()}"
         }
     }
 }
